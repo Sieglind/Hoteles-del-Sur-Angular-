44f3c5bf2b1e5fbd66394598951fb9b42b86a5b4
Cristian Moraleda <cristian.moraleda.-nd@disney.com>
2024-10-29 15:27:52 -0300

    Refactored App Flow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { MyReservationsComponent } from './components/my-reservations/my-reservations.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {ReservationFormComponent} from './components/reservation-form/reservation-form.component';
+import {MyReservationsComponent} from './components/my-reservations/my-reservations.component';
 
 const routes: Routes = [
-  { path: '', component: HeaderComponent, outlet: 'nav'},
-  { path: '', component: HeroComponent, outlet: 'header' },
-  { path: '', component: LandingPageComponent, outlet: 'main', pathMatch: 'full' },
-  { path: '', component: FooterComponent, outlet: 'footer' },
-  { path: 'reservation', component: ReservationFormComponent, outlet: 'main' },
-  { path: 'my-reservations', component: MyReservationsComponent, outlet: 'main' }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: LandingPageComponent},
+  {path: 'reservations', component: MyReservationsComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
