a1e4b5e22325bae2e00a4f6a5ffed7a1310574fe
Sieglind <moraledacd@gmail.com>
2024-11-09 13:10:31 -0300

    Comunicacion mediante eventos

--- src/app/components/services/service/service.component.ts
+++ src/app/components/services/service/service.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PexelService} from '../../../services/pexel.service';
 import {Photo, Video} from 'pexels';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {EventBusService} from '../../../services/event-bus.service';
 
 @Component({
   selector: 'app-service',
@@ -16,13 +17,25 @@ export class ServiceComponent implements OnInit {
 
   constructor(
     private pexeService: PexelService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private eventBusService: EventBusService
   ) {}
 
   async ngOnInit() {
+    await this.getServiceMedia();
+    this.eventBusService.changeService$.subscribe(
+      service => {
+        this.service = service;
+        this.getServiceMedia();
+      }
+    )
+  }
+
+  async getServiceMedia(): Promise<void> {
     this.pexeService.getCollectionMedia(this.service.id).then(collection => {
       if (collection) {
         this.photoMedia = collection.media.filter(collectioNmedia => collectioNmedia.type == 'Photo')
+        this.videoMediaUrls = [];
         collection.media
           .filter(media => media.type === 'Video')
           .forEach(video => {
