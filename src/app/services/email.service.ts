6678a2b7483fa1ce5df7ca8364fc1e06670d3c09
larasalasane <larasalasane@gmail.com>
2024-11-17 20:03:47 -0300

    html-email

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {UserService} from './user.service';
-import {User} from '../models/user.model';
-import {Reservation} from '../models/reservation.model';
+import { UserService } from './user.service';
+import { User } from '../models/user.model';
+import { Reservation } from '../models/reservation.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,28 +14,41 @@ export class EmailService {
 
 
   constructor(
-    private userService: UserService) {}
+    private userService: UserService) { }
 
-  sendNotificationMail(reservation: Reservation){
-    let user : User | null = this.userService.getUserData();
+  sendNotificationMail(reservation: Reservation) {
+    let user: User | null = this.userService.getUserData();
     const template = `
-      <html lang="es">
-        <body>
-          <h1>Confirmación de Reserva</h1>
-          <p>Estimado/a,</p>
-          <p>Su reserva del <strong>{{checkInDate}}</strong> al <strong>{{checkOutDate}}</strong> se ha realizado con éxito.</p>
-          <p>¡Gracias por elegirnos!</p>
-        </body>
-      </html>
+     <html>
+<body>
+<h1>Gracias por elegir a Hoteles del Sur. ¡Estamos deseando alojarte!</h1>
+<h2>Estimado/a {{user.nombre}}:</h2>
+<h3>¡ Nos complace informarte que tu estadia se ha confirmado con el número de Reserva n° {{resevation.id}} !</h3>
+<p strong>Aqui te dejamos los detalles de tu reserva: </p>
+<p>Check-In: {{reservation.checkInDate}}</p>
+<p>Check-Out: {{reservation.checkOutDate}}</p>
+<p>Cantidad de Pasajeros: {{reservation.guests}}</p>
+<p>Tipo de habitación: {{}}</p>
+<p>Total a abonar: {{}}</p>
+<p>Politica: </p>
+<p>- Si cancelas la reserva, el cargo por cancelar será el precio de la primera noche.</p> 
+<p>- Esta reserva no es reembolsable. No se pueden cambiar las fechas de estancia.</p>
+<p>¿Tienes alguna pregunta o petición especial? Contacta con nosotros.</p>
+<h3>¡Qué empiece la cuenta atrás para tus vacaciones!</h3>
+
+
+</body>
+</html>
+
     `;
     if (user) {
       const sendgridClient = require('@sendgrid/mail')
       sendgridClient.setApiKey(this.apiKey);
-      const email =  {
+      const email = {
         to: user.email,
         from: this.companyMail,
         subject: `Confirmacion de Reserva: ${reservation.checkInDate} a ${reservation.checkOutDate}`,
-        text: 'Este mail le llega para ocnfirmar que su reserva se ha realizado con exito',
+        html: '',
       }
     }
   }
